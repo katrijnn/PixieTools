68af8453bb6b7960a67fc6d100f47cf34e718367
Geoffrey Trees <gptrees@gmail.com>
2012-06-23 22:21:02 -0400

    Refactored HistArray and fixed logic.

--- src/pixietools/HistArray.java
+++ src/pixietools/HistArray.java
@@ -1,272 +1,202 @@
 package pixietools;
 
 import java.io.*;
-//import java.util.*;
 
 public class HistArray 
 {
 	public static void main(String[] args)
 	{
-		PixieBinFile myFile = new PixieBinFile("C:\\Users\\Katrijn\\Desktop\\PixieTestFiles\\test_file_0004.bin");
-		myFile.open();
-	
-		int[] ch0Array = new int[32768];
-		int[] ch1Array = new int[32768];
-		int[] ch2Array = new int[32768];
-		int[] ch3Array = new int[32768];
-	
-		// zero out arrays
-		for (int i=0; i < ch0Array.length; i++)
-		{
-			ch0Array[i] = 0;
-		}
+		// Example of how to export a spectrum for ch0 and ch1 with 1024 bins
 		
-		for (int i=0; i < ch1Array.length; i++)
-		{
-			ch1Array[i] = 0;
-		}
+		HistArray newHistArray = new HistArray();
 		
-		for (int i=0; i < ch2Array.length; i++)
-		{
-			ch2Array[i] = 0;
-		}
+		int[] ch0 = new int[32768];
+		int[] ch1 = new int[32768];
+		int[] ch2 = new int[32768];
+		int[] ch3 = new int[32768];
 		
-		for (int i=0; i < ch3Array.length; i++)
-		{
-			ch3Array[i] = 0;
-		}
+		String binFilePath = "/home/gtrees/Desktop/co60.bin";
 		
+		if (!newHistArray.getHistogram(binFilePath, 0, ch0, ch1, ch2, ch3))
+			return;
+		
+		// For testing, lets just compress ch0 and ch1 and output them
+		int[] ch0New = new int[1024];
+		int[] ch1New = new int[1024];
+		
+		if (!newHistArray.getCompressedHistogram(ch0, ch0New))
+			return;
+		
+		if (!newHistArray.getCompressedHistogram(ch1, ch1New))
+			return;
+		
+		String out0Path = "/home/gtrees/Desktop/ch0.txt";
+		String out1Path = "/home/gtrees/Desktop/ch1.txt";
+		
+		newHistArray.writeHistogramToAsciiFile(out0Path, ch0New);
+		newHistArray.writeHistogramToAsciiFile(out1Path, ch1New);
+		
+		System.out.println("Done!");
+	}
+
+	private void writeHistogramToAsciiFile(String outPath, int[] histogram)
+	{
 		try 
 		{
-			System.out.println("Starting HistArray...");
-			String line = null;
-			int userChanNum = 0;
+			FileOutputStream fos = new FileOutputStream(outPath);
+			OutputStreamWriter histFile = new OutputStreamWriter(fos, "UTF-8");
 			
-			FileOutputStream fos = new FileOutputStream("C:\\Users\\Katrijn\\Desktop\\hist_out.txt");
-			OutputStreamWriter hist_out = new OutputStreamWriter(fos, "UTF-8");
-
-			// create header
-			hist_out.write("Energy Histogram Data" + "\n");
-			hist_out.write("---------------------");
-			hist_out.write("\n\n");
-
-			// read string input from console
-			// depending on which channel user requests
-			System.out.println("Please enter channel number between 0 and 3");
-			BufferedReader hist_reader = new BufferedReader(new InputStreamReader(System.in));
-			line = hist_reader.readLine();
-			userChanNum = Integer.parseInt(line);
-
+			for (int i = 0; i < histogram.length; i++)
+			{
+				histFile.write(String.valueOf(histogram[i]) + "\n");
+			}
 			
+			histFile.close();
+			fos.close();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("An error has occured in writeHistogramToAsciiFile: " + e.getMessage());
+		}
+	}
+	
+	private boolean getCompressedHistogram(int[] oldHistogram, int[] newHistogram)
+	{
+		// Define valid sizes
+		int[] validNewBinSizes = {1024, 2048, 4096, 8192, 16384};
+		boolean validBinSize = false;
+		
+		// Only compress histograms that were originally 32768 (for now)
+		if (oldHistogram.length != 32768)
+			return false;
+		
+		// Check all possible values of newBinSize
+		for (int i = 0; i < validNewBinSizes.length; i++)
+		{
+			if (validNewBinSizes[i] == newHistogram.length)
+			{
+				validBinSize = true;
+				break;
+			}
+		}
+
+		// If not valid in bin size, return false
+		if (!validBinSize)
+			return false;
+		
+		// Zero out newHistogram
+		for (int i = 0; i < newHistogram.length; i++)
+		{
+			newHistogram[i] = 0;
+		}
+		
+		// Get scaleFactor
+		double scaleFactor = ((double) newHistogram.length) / ((double) oldHistogram.length);
+		
+		// Create new histogram
+		for (int i = 0; i < oldHistogram.length; i++)
+		{
+			int newBin = (int)Math.floor(scaleFactor * (double)i);
+			newHistogram[newBin] += oldHistogram[i];
+		}
+		
+		return true;
+	}
+
+	private boolean getHistogram(String binFilePath, int moduleId, int[] ch0, int[] ch1, int[] ch2, int[] ch3)
+	{
+		PixieBinFile myFile = new PixieBinFile(binFilePath);
+		
+		// Open file and return false if open returns false
+		if (!myFile.open())
+			return false;
+		
+		// Check to see if moduleId is 0 or 1
+		if ((moduleId != 0) && (moduleId != 1))
+			return false;
+		
+		// Check to see arrays are correct size
+		if (ch0.length != 32768) return false;
+		if (ch1.length != 32768) return false;
+		if (ch2.length != 32768) return false;
+		if (ch3.length != 32768) return false;
+	
+		// Zero out arrays
+		for (int i=0; i < ch0.length; i++)
+		{
+			ch0[i] = 0;
+		}
+		
+		for (int i=0; i < ch1.length; i++)
+		{
+			ch1[i] = 0;
+		}
+		
+		for (int i=0; i < ch2.length; i++)
+		{
+			ch2[i] = 0;
+		}
+		
+		for (int i=0; i < ch3.length; i++)
+		{
+			ch3[i] = 0;
+		}
+		
+		// Add +1 for respective channel and bin
+		try 
+		{	
 			for (boolean bBuffer = myFile.moveFirstBuffer(); bBuffer; bBuffer = myFile.moveNextBuffer())
 			{
+				// Check to see if this buffer belongs to moduleId
+				if (myFile.getBufferModuleNumber() != moduleId)
+					continue;
 				
-				// Loop over all events and print them to text file
 				for (boolean bEvent = myFile.moveFirstEvent(); bEvent; bEvent = myFile.moveNextEvent())
 				{
-					// loop over each channel hit in this event
 					for (boolean bChannel = myFile.moveFirstChannel(); bChannel; bChannel = myFile.moveNextChannel())
 					{
 						int chanNum = myFile.getEventChannel();
 						int chanEnergy = myFile.getEventEnergy();
-
+	
 						if (chanEnergy < 32768)
 						{
 							if (chanNum == 0)
 							{
-								ch0Array[chanEnergy]= ch0Array[chanEnergy] + 1;
+								ch0[chanEnergy]= ch0[chanEnergy] + 1;
 							}
 								
 							else if (chanNum == 1)
 							{
-								ch1Array[chanEnergy] = ch1Array[chanEnergy] + 1;
+								ch1[chanEnergy] = ch1[chanEnergy] + 1;
 							}
 								
 							else if (chanNum == 2)
 							{
-								ch2Array[chanEnergy] = ch2Array[chanEnergy] + 1;
+								ch2[chanEnergy] = ch2[chanEnergy] + 1;
 							}
 								
 							else if (chanNum == 3)
 							{
-								ch3Array[chanEnergy] = ch3Array[chanEnergy] + 1;
+								ch3[chanEnergy] = ch3[chanEnergy] + 1;
 							}
-							
 						}
-						
 						else 
 						{
 							continue;
 						}
-						
 					}
 				}
 			}
-			
-			// iterate over arrays to write to output file
-
-			if (userChanNum == 0)
-			{
-				for(int i=0; i < ch0Array.length; i++)
-				{
-					hist_out.write(String.valueOf(ch0Array[i]) + "\n");
-				}
-			}
-				
-			if (userChanNum == 1)
-			{
-				for(int i=0; i < ch1Array.length; i++)
-				{
-					hist_out.write(String.valueOf(ch1Array[i]) + "\n");
-				}
-			}
-				
-			if (userChanNum == 2)
-			{
-				for(int i=0; i< ch2Array.length; i++)
-				{
-					hist_out.write(String.valueOf(ch2Array[i]) + "\n");
-				}
-			}
-				
-			if (userChanNum == 3)
-			{
-				for(int i=0; i < ch3Array.length; i++)
-				{
-					hist_out.write(String.valueOf(ch3Array[i]) + "\n");
-				}
-			}
-			
-			hist_out.close();
-			
-			
-			String compYN_line = null;
-			int compYN = 0;
-			
-			System.out.println("Do you wish to compress this histogram data? Please enter 0 for yes, 1 for no" + "\n");
-			BufferedReader compYN_reader = new BufferedReader(new InputStreamReader(System.in));
-			compYN_line = compYN_reader.readLine();
-			compYN = Integer.parseInt(compYN_line);
-			
-			if (compYN == 0)
-			{
-				FileOutputStream fos1 = new FileOutputStream("C:\\Users\\Katrijn\\Desktop\\compHist_out.txt");
-				OutputStreamWriter compHist_out = new OutputStreamWriter(fos1, "UTF-8");
-				
-				// create header
-				compHist_out.write("Compressed Energy Histogram Data" + "\n");
-				compHist_out.write("---------------------");
-				compHist_out.write("\n\n");
-				
-				int[] validNewBinSizes = {1024, 2048, 4096, 8192, 16384};
-				boolean validBinSize = false;
-
-				
-				String newBin_line = null;
-				int newBinSize = 32768;
-				
-				System.out.println("Please enter new Bin Size (1024, 2048, 4096, 8192, 16384) " + "\n");
-				BufferedReader newBinSize_reader = new BufferedReader(new InputStreamReader(System.in));
-				newBin_line = newBinSize_reader.readLine();
-				newBinSize = Integer.parseInt(newBin_line);
-				
-				// check all possible values of newBinSize to make sure in array
-				for (int i = 0; i < validNewBinSizes[i]; i++)
-				{
-					if (validNewBinSizes[i] == newBinSize)
-						System.out.println("yes");
-						continue;	
-				}
-				System.out.println("got stuck here");
-
-				// if not valid in bin size, return false
-				if (!validBinSize)
-					validBinSize = false;
-				
-
-				// get scale factor
-				double scaleFactor = ((double)newBinSize)/32768;
-				
-			 	// create new Histogram
-				int[] compHist = new int[newBinSize];
-				for (int i = 0; i < compHist.length; i++)
-				{
-					compHist[i] = 0;
-				}
-				
-				// convert unscaled histogram to scaled histogram
-				if (userChanNum == 0) 
-				{
-
-					for (int i = 0; i < ch0Array.length; i++)
-					{
-
-						int newBin = (int)Math.floor(scaleFactor * (double)i);
-						compHist[newBin] += ch0Array[i];
-						for (int j=0; j < compHist.length; j++)
-						{
-							compHist_out.write(String.valueOf(compHist[j])+ "\n");
-						}
-					}
-				}
-				else if (userChanNum == 1)
-				{
-					for (int i = 0; i < ch1Array.length; i++)
-					{
-						int newBin = (int)Math.floor(scaleFactor * (double)i);
-						compHist[newBin] += ch1Array[i];
-						for (int j = 0; i < compHist.length; j++)
-						{
-							compHist_out.write(String.valueOf(compHist[j]) + "\n");
-						}
-					}
-				}
-				else if (userChanNum == 2)
-				{
-					for (int i = 0; i < ch2Array.length; i++)
-					{
-						int newBin = (int)Math.floor(scaleFactor * (double)i);
-						compHist[newBin] += ch2Array[i];
-						for (int j = 0; j < compHist.length; j++)
-						{
-							compHist_out.write(String.valueOf(compHist[j]) + "\n");
-
-						}
-					}
-				}
-				else if (userChanNum == 3)
-				{
-					for (int i = 0; i < ch3Array.length; i++)
-					{
-						int newBin = (int)Math.floor(scaleFactor * (double)i);
-						compHist[newBin] += ch3Array[i];
-						for (int j = 0; j < compHist.length; j++)
-						{
-							compHist_out.write(String.valueOf(compHist[j]) + "\n");
-
-						}
-					}
-				}
-				
-				compHist_out.close();
-			
-			}
-			else if (compYN == 1)
-			{
-				System.out.println("Finished HistArray...");
-			}
-			
-			else if (compYN > 1)
-			{
-				System.out.println("Error! Please enter 0 for yes and 1 for no");
-			}
-			
-		}	
+		}
 		catch (Exception e)
 		{
-			System.out.println("An error has occured in arrays!");
+			System.out.println("An error has occured in getHistogram: " + e.getMessage());
+			myFile.close();
+			return false;
 		}
 		
 		myFile.close();
+		
+		return true;
 	}
 }
