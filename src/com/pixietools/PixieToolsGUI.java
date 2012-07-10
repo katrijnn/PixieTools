6eee4cfd3a71d35b24f22af9b0738515e30b4d2d
Geoffrey Trees <gptrees@gmail.com>
2012-07-09 22:51:22 -0400

    Added GUI project file

---
+++ src/com/pixietools/PixieToolsGUI.java
@@ -0,0 +1,44 @@
+package com.pixietools;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PixieToolsGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PixieToolsGUI window = new PixieToolsGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PixieToolsGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+
+}
